41c46670366d0f66174ae28d8d07e217348c42e0
ThellaVamshidhar <thellavamshidhar244@gmail.com>
2024-03-26 10:30:03 +0530

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import CloseIcon from '@mui/icons-material/Close';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddIcon from '@mui/icons-material/Add';
+import ChecklistIcon from '@mui/icons-material/Checklist';
+import Tooltip from '@mui/material/Tooltip';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [listItems, setListItems] = useState([]);
+
+  const handleAdd = () => {
+    if (inputValue.trim() !== '') {
+      setListItems([...listItems, { text: inputValue, isFinished: false }]);
+      setInputValue('');
+    } else {
+      alert("Please enter something");
+    }
+  };
+
+  const handleClear = () => {
+    setListItems([]);
+  };
+
+  const handleEdit = (index) => {
+    const newText = prompt("Enter new text:");
+    if (newText !== null) {
+      const updatedItems = [...listItems];
+      updatedItems[index].text = newText;
+      setListItems(updatedItems);
+    }
+  };
+
+  const handleFinish = (index) => {
+    const updatedItems = [...listItems];
+    updatedItems.splice(index, 1);
+    setListItems(updatedItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-div'>
+      <h1>Todo App< ChecklistIcon/></h1>
+      <div className="input-container">
+        <input
+          type="text"
+          className='input-el'
+          placeholder='Enter task here'
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <Tooltip title="Add" arrow><button onClick={handleAdd} className='btn-add'><AddIcon /></button></Tooltip>
+        <Tooltip title="Clear all tasks" arrow><button className='btn-clearall' onClick={handleClear}>< DeleteIcon/></button></Tooltip>
+      </div>
+      
+      <ul className="list">
+        {listItems.map((item, index) => (
+          <li key={index} className={item.isFinished ? "finished" : ""}>
+            {item.text}
+            <div >
+            <Tooltip title="Edit" arrow><button className='btn-edit' onClick={() => handleEdit(index)}><ModeEditIcon fontSize='small'/></button></Tooltip>
+            <Tooltip title="Finish" arrow><button className='btn-finish' onClick={() => handleFinish(index)}><CloseIcon fontSize='small'/></button></Tooltip></div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
